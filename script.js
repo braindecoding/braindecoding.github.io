8588a4ded0feaa72899ec2b563264ac931ed2d66
Rolly Maulana Awangga <awangga@gmail.com>
2024-03-28 16:40:48 +0700

    sip

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 document.getElementById('contactBtn').addEventListener('click', function() {
     alert('Thank you for your interest! Please send us an email at awangga@gmail.com.');
 });
+
+document.querySelectorAll('.btn-show-bibtex').forEach(button => {
+    button.addEventListener('click', () => {
+        const bibtexEntry = button.nextElementSibling;
+        bibtexEntry.style.display = bibtexEntry.style.display === 'none' ? 'block' : 'none';
+    });
+});
