5bdaf7ca9ef95225c2aa970a915c5480a1bec026
Rolly Maulana Awangga <awangga@gmail.com>
2024-03-28 16:15:30 +0700

    update

---
+++ script.js
@@ -0,0 +1,3 @@
+document.getElementById('contactBtn').addEventListener('click', function() {
+    alert('Thank you for your interest! Please send us an email at contact@braindecodingresearch.com.');
+});
